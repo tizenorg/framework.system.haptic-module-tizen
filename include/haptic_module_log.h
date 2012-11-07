7fd9132ee1d60e1f658daad8e0fac11fe8159e99
j-zero.hwang <j-zero.hwang@samsung.com>
2012-11-07 18:34:46 +0900

    Implement haptic module for TIZEN
    Change-Id: Id5ddcd75fcb6cf1ac6c9f4c13d0e6bb3a12baeff

---
+++ include/haptic_module_log.h
@@ -0,0 +1,37 @@
+/*
+ *  haptic-module-tizen
+ *
+ * Copyright (c) 2011 - 2012 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact: Jae-young Hwnag <j-zero.hwang@samsung.com>
+ *
+ * PROPRIETARY/CONFIDENTIAL
+ *
+ * This software is the confidential and proprietary information of
+ * SAMSUNG ELECTRONICS ("Confidential Information"). You agree and acknowledge
+ * that this software is owned by Samsung and you shall not disclose
+ * such Confidential Information and shall use it only in accordance with the
+ * terms of the license agreement you entered into with SAMSUNG ELECTRONICS.
+ * SAMSUNG make no representations or warranties about the suitability
+ * of the software, either express or implied, including but not limited
+ * to the implied warranties of merchantability, fitness for a particular
+ * purpose,
+ * by licensee arising out of or releated to this software.
+*/
+
+#ifndef __HAPTIC_MODULE_LOG_H__
+#define __HAPTIC_MODULE_LOG_H__
+
+#define FEATURE_HAPTIC_MODULE_DLOG
+
+#ifdef FEATURE_HAPTIC_MODULE_DLOG
+    #define LOG_TAG "HAPTIC_MODULE"
+    #include <dlog.h>
+    #define MODULE_LOG(fmt, args...)       SLOGD("<%s:%4d> " fmt "\n", __func__, __LINE__, ##args)
+    #define MODULE_ERROR(fmt, args...)     SLOGE("<%s:%4d> " fmt "\n", __func__, __LINE__, ##args)
+#else
+    #define MODULE_LOG(x, ...)
+    #define MODULE_ERROR(x, ...)
+#endif
+
+#endif //__HAPTIC_MODULE_LOG_H__
---
+++ packaging/haptic-module-tizen.spec
@@ -0,0 +1,42 @@
+#sbs-git:slp/pkgs/d/devman devman 0.1.6 5bf2e95e0bb15c43ff928f7375e1978b0accb0f8
+Name:       haptic-module-tizen
+Summary:    Haptic Module library
+Version:    0.1.0
+Release:    0
+Group:      System/Libraries
+License:    LGPL 2.1
+Source0:    %{name}-%{version}.tar.gz
+BuildRequires: cmake
+BuildRequires: pkgconfig(dlog)
+BuildRequires: pkgconfig(vconf)
+BuildRequires: pkgconfig(haptic-plugin)
+BuildRequires: pkgconfig(devman)
+
+Requires(post): /sbin/ldconfig
+Requires(postun): /sbin/ldconfig
+
+%description
+
+%prep
+%setup -q
+
+%build
+cmake . -DCMAKE_INSTALL_PREFIX=%{_prefix}
+make %{?jobs:-j%jobs}
+
+%install
+rm -rf %{buildroot}
+%make_install
+
+%post -p /sbin/ldconfig
+
+%postun -p /sbin/ldconfig
+
+%files
+%{_libdir}/haptic/libhaptic-module.so
+%{_bindir}/test1
+
+%changelog
+* Fri Nov 2 2012 - Jae-young Hwang <j-zero.hwang@samsung.com>
+- Implement haptic-module-tizen
+- Tag : haptic-module-tizen_0.1.0-0
